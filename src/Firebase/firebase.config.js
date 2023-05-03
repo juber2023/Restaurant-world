2f3099709caa09121765542618f462eb014df47c
juber2023 <juber.eco@gmail.com>
2023-05-03 22:55:22 +0600

    environment variable done

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,18 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCeR57z8up_HPswxX526QfxY2kG2qUyHQA",
-  authDomain: "food-lovers-3b485.firebaseapp.com",
-  projectId: "food-lovers-3b485",
-  storageBucket: "food-lovers-3b485.appspot.com",
-  messagingSenderId: "867755350146",
-  appId: "1:867755350146:web:183de501b911ea3053fc9b"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId ,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app
